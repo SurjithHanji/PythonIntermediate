ade4c50c08554cceffe858225d8f6319080044cc
SurjithHanji <surjithhanji12345@gmail.com>
2025-04-06 13:35:41 +0530

    Add files via upload

---
+++ s1.py
@@ -0,0 +1,16 @@
+set1={1,2,3,4,5,5,4,3,2,1,0}
+print(set1)
+
+set1.add(7)
+set1.add(8)
+set1.add(9)
+print(set1)
+
+print(set1.pop())
+
+print(set1)
+
+for i in set1:
+    print(i)
+if 7 in set1:
+    print(True)
